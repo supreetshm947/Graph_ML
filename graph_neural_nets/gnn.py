bbeec3f663730ad553c6ec1e50565c42b1d3314a
Supreet <supreet.sharma@rwth-aachen.de>
2024-09-05 10:22:29 +0200

    adding graph neural networks

--- graph_neural_nets/gnn.py
+++ graph_neural_nets/gnn.py
@@ -1,33 +1,39 @@
 from torch import nn
 import torch.nn.functional as f
 from torch_geometric.utils import scatter
+from torch_geometric.nn.norm import BatchNorm
 
 
 class GCNLayer(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout_prob=0.5):
         super(GCNLayer, self).__init__()
         self.Linear = nn.Linear(in_channels, out_channels)
         self.in_channels = in_channels
         self.out_channels = out_channels
-
+        self.BatchNorm = BatchNorm(out_channels)
+        self.dropout = nn.Dropout(dropout_prob)
 
     def forward(self, x, edge_index):
         x = self.Linear(x)
         in_node, out_node = edge_index
         num_nodes = x.size(0)
         out = scatter(x[in_node], out_node, dim=0, dim_size=num_nodes, reduce="mean")
-        return f.relu(out)
+        out = self.BatchNorm(out)
+        out = f.relu(out)
+        return self.dropout(out)
 
 
 class GNN(nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super(GNN, self).__init__()
         self.layer1 = GCNLayer(in_channels, hidden_channels)
-        self.layer2 = GCNLayer(hidden_channels, out_channels)
+        self.layer2 = GCNLayer(hidden_channels, hidden_channels)
+        self.layer3 = GCNLayer(hidden_channels, out_channels)
 
     def forward(self, x, edge_index, batch):
         x = self.layer1(x, edge_index)
         x = self.layer2(x, edge_index)
+        x = self.layer3(x, edge_index)
         # batch contains index value which corresponds to which graph x belongs to
         x = scatter(x, batch, dim=0, reduce="mean")
         return x
--- graph_neural_nets/train_gnn.py
+++ graph_neural_nets/train_gnn.py
@@ -60,12 +60,16 @@ def main():
 
     train_loader = prepare_data("../datasets/zinc/train.jsonl", device)
     val_loader = prepare_data("../datasets/zinc/val.jsonl", device)
+    test_loader = prepare_data("../datasets/zinc/test.jsonl", device)
 
     for epoch in range(NUM_EPOCHS):
-        train_loss = train(model, train_loader , optimizer, criterion, device)
+        train_loss = train(model, train_loader, optimizer, criterion, device)
         val_loss = validate(model, val_loader, criterion, device)
         print(f'Epoch {epoch + 1}/{NUM_EPOCHS}, '
               f'Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}')
+
+    test_loss = validate(model, test_loader, criterion, device)
+    print(f'Test Loss: {test_loss:.4f}')
 
 
 if __name__ == "__main__":
